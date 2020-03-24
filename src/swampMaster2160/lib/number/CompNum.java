574a03f9a27f3c29359073e49142b0e2a08eaaaf
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2020-03-24 15:22:08 +1300

    Add files via upload

--- src/swampMaster2160/lib/number/CompNum.java
+++ src/swampMaster2160/lib/number/CompNum.java
@@ -3,28 +3,69 @@ package swampMaster2160.lib.number;
 public class CompNum {
 	public Object r, i;
 	
+	/**
+	 * Create a new complex number with a null real and imaginary part.
+	 */
+	
 	public CompNum() {
 		
 	}
+	
+	/**
+	 * Create a new complex number with a set real and imaginary part.
+	 * 
+	 * @param r - the real part of the number.
+	 * @param i - the imaginary part of the number.
+	 */
 	
 	public CompNum(Object r, Object i) {
 		this.r = r;
 		this.i = i;
 	}
 	
+	/**
+	 * Converts a vec2 coordinate to a complex number, with (x, y) being converted to x + yi.
+	 * 
+	 * @param number - the vec2 coordinate to be converted.
+	 * @see Vec2
+	 */
+	
 	public CompNum(Vec2 number) {
 		this.r = number.x;
 		this.i = number.y;
 	}
+	
+	/**
+	 * Converts a polar coordinate to a complex number.
+	 * 
+	 * @param number - the polar coordinate to be converted.
+	 * @see Polar
+	 */
 	
 	public CompNum(Polar number) {
 		this.r = (double)number.r * Math.cos((double)number.t);
 		this.i = (double)number.r * Math.sin((double)number.t);
 	}
 	
+	/**
+	 * Returns true if and only if the real parts of the 2 numbers are equal and the imaginary parts of the 2 numbers are equal. Else will return false.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return the equality of the 2 complex numbers.
+	 */
+	
 	public static boolean isEqual(CompNum a, CompNum b) {
 		return (a.r == b.r && a.i == b.i);
 	}
+	
+	/**
+	 * Adds the 2 complex numbers and returns the sum.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return complex sum of a + b.
+	 */
 	
 	public static CompNum add(CompNum a, CompNum b) {
 		a.r = (double)a.r + (double)b.r;
@@ -33,12 +74,28 @@ public class CompNum {
 		return a;
 	}
 	
+	/**
+	 * Subtracts b from a and returns the difference.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return complex difference of a - b.
+	 */
+	
 	public static CompNum sub(CompNum a, CompNum b) {
 		a.r = (double)a.r - (double)b.r;
 		a.i = (double)a.i - (double)b.i;
 		
 		return a;
 	}
+	
+	/**
+	 * Multiplies the 2 complex numbers and returns the product.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return complex product of a × b.
+	 */
 	
 	public static CompNum mult(CompNum a, CompNum b) {
 		CompNum number = new CompNum();
@@ -49,23 +106,30 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Divides a by b and returns the result.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return complex result of a ÷ b.
+	 */
+	
 	public static CompNum div(CompNum a, CompNum b) {
 		CompNum number = new CompNum();
 
 		number.r = ((((double)a.r * (double)b.r) + ((double)a.i * (double)b.i)) / (((double)b.r * (double)b.r) + (double)b.i * (double)b.i));
 		number.i = ((((double)a.i * (double)b.r) - ((double)a.r * (double)b.i)) / (((double)b.r * (double)b.r) + (double)b.i * (double)b.i));
 		
-		/*if((double)a.r < 0 ^ (double)a.i < 0) number.r = 0 - (double)number.r;
-		if((double)b.r < 0 ^ (double)b.i < 0) number.r = 0 - (double)number.r;
-		
-		if((double)a.r < 0 ^ (double)a.i < 0) number.i = 0 - (double)number.i;
-		if((double)b.r < 0 ^ (double)b.i < 0) number.i = 0 - (double)number.i;*/
-		
-		//number.r = 0 - (double)number.r;
-		//number.i = 0 - (double)number.i;
-		
 		return number;
 	}
+	
+	/**
+	 * Computes the complex exponent a<sup>b</sup> and returns the result.
+	 * 
+	 * @param a - number 1.
+	 * @param b - number 2.
+	 * @return complex result of exponent a<sup>b</sup>.
+	 */
 	
 	public static CompNum pow(CompNum a, int b) {
 		CompNum number = new CompNum(1., 0.);
@@ -77,6 +141,12 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Computes the complex sine of z and returns the result.
+	 * 
+	 * @return complex sine of z.
+	 */
+	
 	public CompNum sin() {
 		CompNum number = new CompNum(0., 0.);
 		
@@ -85,6 +155,12 @@ public class CompNum {
 		
 		return number;
 	}
+	
+	/**
+	 * Computes the complex cosine of z and returns the result.
+	 * 
+	 * @return complex cosine of z.
+	 */
 	
 	public CompNum cos() {
 		CompNum number = new CompNum(0., 0.);
@@ -95,13 +171,31 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Computes the complex tangent of z and returns the result.
+	 * 
+	 * @return complex tangent of z.
+	 */
+	
 	public CompNum tan() {
 		return CompNum.div(this.sin(), this.cos());
 	}
 	
+	/**
+	 * Computes the complex cotangent of z and returns the result.
+	 * 
+	 * @return complex cotangent of z.
+	 */
+	
 	public CompNum cot() {
 		return CompNum.div(this.cos(), this.sin());
 	}
+	
+	/**
+	 * Computes the complex secant of z and returns the result.
+	 * 
+	 * @return complex secant of z.
+	 */
 	
 	public CompNum sec() {
 		CompNum number = CompNum.div(new CompNum(1., 0.), this.cos());
@@ -113,6 +207,12 @@ public class CompNum {
 		
 		return number;
 	}
+	
+	/**
+	 * Computes the complex cosecant of z and returns the result.
+	 * 
+	 * @return complex cosecant of z.
+	 */
 	
 	public CompNum csc() {
 		CompNum number = CompNum.div(new CompNum(1., 0.), this.sin());
@@ -126,6 +226,12 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Computes the square root of z and returns the result.
+	 * 
+	 * @return complex square root of z.
+	 */
+	
 	public CompNum sqrt() {
 		CompNum number = new CompNum();
 		
@@ -137,9 +243,21 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Computes the square of z and returns the result.
+	 * 
+	 * @return the result of complex exponent z<sup>2</sup>.
+	 */
+	
 	public CompNum sq() {
 		return(CompNum.mult(this, this));
 	}
+	
+	/**
+	 * Computes the complex conjugate of z and returns the result.
+	 * 
+	 * @return the complex conjugate of z.
+	 */
 	
 	public CompNum conj() {
 		CompNum number = this;
@@ -147,14 +265,97 @@ public class CompNum {
 		return number;
 	}
 	
+	/**
+	 * Computes the complex reciprocal of z and returns the result.
+	 * 
+	 * @return the complex reciprocal of z or 1 ÷ z.
+	 */
+	
 	public CompNum recip() {
 		return CompNum.div(new CompNum(1., 0.), this);
 	}
 	
-	public String phrase() {
-		if((double)this.i < 0) return this.r + " - " + (0. - (double)this.i) + "i";
-		else return this.r + " + " + this.i + "i";
+	/**
+	 * Computes the complex exponent e<sup>z</sup> and returns the result.
+	 * 
+	 * @return the result of complex exponent e<sup>z</sup>.
+	 */
+	
+	public CompNum exp() {
+		return CompNum.mult(new CompNum(Math.pow(Math.E, (double)this.r), 0.), CompNum.add(new CompNum(Math.cos((double)this.i), 0.), CompNum.mult(new CompNum(0., 1.), new CompNum(Math.sin((double)this.i), 0.))));
 	}
+	
+	/**
+	 * Computes the complex natural logarithm of z and returns the result.
+	 * 
+	 * @return the complex natural logarithm of z.
+	 */
+	
+	public CompNum ln() {
+		CompNum out = new CompNum();
+		
+		if((double)this.r >= 0) out = new CompNum(Math.log(Math.pow((double)this.r, 2) + Math.pow((double)this.i, 2)) / 2, Math.atan((double)this.i / (double)this.r));
+		else if((double)this.i >= 0) out = new CompNum(Math.log(Math.pow((double)this.r, 2) + Math.pow((double)this.i, 2)) / 2, Math.PI + Math.atan((double)this.i / (double)this.r));
+		else out = new CompNum(Math.log(Math.pow((double)this.r, 2) + Math.pow((double)this.i, 2)) / 2, Math.atan((double)this.i / (double)this.r) - Math.PI);
+		
+		return out;
+	}
+	
+	/**
+	 * Computes the complex base 10 logarithm of z and returns the result.
+	 * 
+	 * @return the complex base 10 logarithm of z.
+	 */
+	
+	public CompNum log() {
+		return CompNum.div(this.ln(), new CompNum(2.302585092994046, 0.));
+	}
+	
+	/**
+	 * Computes the complex base b logarithm of z and returns the result.
+	 * 
+	 * @param b - base
+	 * @return the complex base b logarithm of z.
+	 */
+	
+	public CompNum logb(CompNum b) {
+		return CompNum.div(this.ln(), b.ln());
+	}
+	
+	/**
+	 * Converts z to a java string.
+	 * 
+	 * @return z as a java string.
+	 * @see String
+	 */
+	
+	public String toString() {
+		String out = new String();
+		
+		if((double)this.r != 0) out += this.r;
+		
+		if((double)this.r != 0 && (double)this.i > 0) out += " + ";
+		if((double)this.r != 0 && (double)this.i < 0) out += " - ";
+		
+		if((double)this.i > 0 && (double)this.i != 1) out += i;
+		
+		if((double)this.i < 0 && (double)this.r == 0) out += "-";
+		if((double)this.i < 0 && (double)this.i != -1) out += Math.abs((double)i);
+		
+		if((double)this.i != 0) out += "i";
+		if((double)this.r == 0 && (double)this.i == -1) out = "-i";
+		
+		if((double)this.r == 0 && (double)this.i == 0) out = "0";
+		
+		return out;
+	}
+	
+	/**
+	 * Converts z to a vec2 coordinate.
+	 * 
+	 * @return z as a vec2 coordinate.
+	 * @see String
+	 */
 	
 	public Vec2 toVec2() {
 		Vec2 number = new Vec2();
@@ -162,6 +363,13 @@ public class CompNum {
 		number.y = this.i;
 		return number;
 	}
+	
+	/**
+	 * Converts z to a polar coordinate.
+	 * 
+	 * @return z as a polar coordinate.
+	 * @see String
+	 */
 	
 	public Polar toPolar() {
 		Polar number = new Polar();
